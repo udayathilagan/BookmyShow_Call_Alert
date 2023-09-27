567ab299798b51b49f2a752964c15b4b33386f70
Apurv <apurv@trackabox.co>
2023-09-27 12:55:44 +0530

    initial

---
+++ Main.py
@@ -0,0 +1,46 @@
+from zenrows import ZenRowsClient
+from twilio.rest import Client
+import time
+from data import credentials
+client = ZenRowsClient(credentials["zenid"])
+
+account_sid = credentials["account_sid"]
+auth_token = credentials["auth_token"]
+phone_No=credentials["phone_No"]
+numberSID=credentials["numberSID"]
+
+url = "https://in.bookmyshow.com/sports/india-vs-australia-icc-mens-cwc-2023/ET00367551"
+
+
+def Call_buddy(delay):
+    try:
+        time.sleep(delay)
+        client = Client(account_sid, auth_token)
+        call = client.calls.create(
+                                url='http://demo.twilio.com/docs/voice.xml',
+                                to='+ 919999999999',
+                                from_=phone_No
+                            )
+        print(call.sid)
+    except Exception as df:
+        print(df)
+
+
+def The_Main():
+    while True:
+        time.sleep(10)
+        try:
+            response = client.get(url)
+            rep=response.text
+            if response.status_code == 200 :
+                if "coming-soon-button"  in rep:
+                    print("Booking not yet open")
+                else :
+                    print("Time To Book")
+                    Call_buddy(0)
+                    time.sleep(60)
+        except Exception as df:
+            print(df)
+
+if __name__ == "__main__":
+    The_Main()  
---
+++ data.py
@@ -0,0 +1,7 @@
+credentials={"account_sid":"Afgfgfc8fsdsd40ac6das1e979sdds",
+"auth_token": "839128dfdff2bbaa307a46dfdfdff3",
+"phone_No":"+14655348448",
+"numberSID":"PN67a54esesdfsdf2da00dfdf6dfdfc283",
+"zenid":"f334234dsfdff028d0dfdadfdfe2a4068"}
+
+# Replace youre credentials above

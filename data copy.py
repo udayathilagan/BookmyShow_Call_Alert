2a45e7894e5ee3426ff9a1b8bced3e4aae881bcb
Apurv <apurv@trackabox.co>
2023-09-27 13:00:40 +0530

    del

--- data copy.py
+++
@@ -1,5 +0,0 @@
-credentials={"account_sid":"ACc8f0c40ac67744891e979ab651a3f105",
-"auth_token": "839128f2bbaa307a46d4d9cf66f6cdf3",
-"phone_No":"+14055432488",
-"numberSID":"PN67a54ee35ab5a7f97c2da008b6a1c283",
-"zenid":"f3a6b16c1da5e70f028d0ad425fef5fece2a4068"}
